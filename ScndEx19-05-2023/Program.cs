a0581d88ec17df1e07bf426dae20510ae1bccebb
Timur Karimov <tationkar@gmail.com>
2023-05-19 22:14:51 +0300

    Thrd Ex

--- ScndEx19-05-2023/Program.cs
+++ ScndEx19-05-2023/Program.cs
@@ -1,12 +1,9 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите первое число:");
 int a = int.Parse(Console.ReadLine());
-
 Console.WriteLine("Введите второе число:");
 int b = int.Parse(Console.ReadLine());
-
 int b2 = Convert.ToInt32(Math.Pow(b, 2));
-
 if(a==b2) 
 {
     Console.WriteLine($"Число {a} является квадратом {b}");
---
+++ ThrdEx19-05-2023/Program.cs
@@ -0,0 +1,21 @@
+﻿/*3. Напишите программу, которая будет выдавать название 
+дня недели по заданному номеру.
+
+3 -> Среда
+5 ->  Пятница*/
+
+Console.Clear();
+Console.WriteLine("Введите число от 1 до 7:");
+int day = int.Parse(Console.ReadLine());
+if (day >= 1 && day <=7)
+{
+    if (day == 1) Console.WriteLine("Понедельник");
+    if (day == 2) Console.WriteLine("Вторник");
+    if (day == 3) Console.WriteLine("Среда");
+    if (day == 4) Console.WriteLine("Четверг");
+    if (day == 5) Console.WriteLine("Пятница");
+    if (day == 6) Console.WriteLine("Суббота");
+    if (day == 7) Console.WriteLine("Воскресенье");
+} else {
+    Console.WriteLine("Введено неправильное число, поробуйте заново");
+}
