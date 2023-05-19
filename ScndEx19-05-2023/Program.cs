97844eadc5c16dc2fb0fac2944a2bab37221660d
Timur Karimov <tationkar@gmail.com>
2023-05-19 21:28:03 +0300

    Scnd Ex

---
+++ ScndEx19-05-2023/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+int b2 = Convert.ToInt32(Math.Pow(b, 2));
+
+if(a==b2) 
+{
+    Console.WriteLine($"Число {a} является квадратом {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a} НЕ является квадратом {b}");
+}
