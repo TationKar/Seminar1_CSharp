2f1a026c7f1a14385a4e3f73f93468efbe67c6af
Timur Karimov <tationkar@gmail.com>
2023-05-19 21:06:39 +0300

    First Ex

---
+++ FirstEx19-05-2023/Program.cs
@@ -0,0 +1,21 @@
+﻿/***Задача 0:** 
+
+**Условие:**
+
+Напишите программу, которая на вход принимает число и 
+выдаёт его квадрат (число умноженное на само себя).
+
+**Например:**
+
+4 -> 16
+-3 -> 9
+-7 -> 49*/
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+//Решение математическим путем
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} рaвен {sqr}");
+//Решение с использованием библиотеки 
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} рaвен {sqr1}");
